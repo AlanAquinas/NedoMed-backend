ad7669b87284965575f9fc9d71cbd8e673fc5d1a
Arslan <alan.aquinasss@gmail.com>
2024-05-16 10:42:39 +0500

    correct appointment book

--- api/urls.py
+++ api/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('patient/<int:pk>/', PatientDataView.as_view(), name='patient-detail'),
     path('doctor/<int:pk>/', DoctorDataView.as_view(), name='doctor-detail'),
-    path('doctor/<int:doctor_id>/appointment/', ScheduleAppointmentView.as_view(), name='schedule-appointment'),
+    # path('doctor/<int:doctor_id>/appointment/', ScheduleAppointmentView.as_view(), name='schedule-appointment'),
     path('appointments/', UserAppointmentListView.as_view(), name='user_appointments'),
-    path('appointments/create/', AppointmentCreateView.as_view(), name='appointment_create'),
+    path('appointments/create/', ScheduleAppointmentView.as_view(), name='appointment_create'),
     path('appointments/<int:id>/', AppointmentDetailView.as_view(), name='appointment_detail')
 ]
